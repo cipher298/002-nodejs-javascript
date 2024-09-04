f03fcc4085f9d6d2eb1c382789cc511a64dc2bd3
Cipher <Cipher>
2024-09-04 19:18:22 +0700

    step 9

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -21,12 +21,6 @@ const getUser = async (req, res) => {
   return res.status(200).json(data);
 };
 
-const handleLogin = async (req, res) => {
-  const { email, password } = req.body;
-  const data = await loginService(email, password);
-  return res.status(200).json(data);
-};
-
 module.exports = {
   createUser,
   handleLogin,
